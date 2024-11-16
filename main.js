c7cdace11ff0d52a29b4cf0ec09a8194bf526a44
tomasAndrad3 <andrad24101@gmail.com>
2024-11-16 13:05:15 -0300

    main.js

--- main.js
+++ main.js
@@ -89,6 +89,19 @@
   srRight.reveal('.form-control',{delay: 100})
   
 
+// Seleccionar todos los bloques de proyectos
+const projectBoxes = document.querySelectorAll('.project-box');
+
+// Agregar evento de clic a cada bloque
+projectBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const url = box.getAttribute('data-url'); // Obtener la URL
+        if (url) {
+            window.open(url, '_blank'); // Abrir la URL en una nueva pestaña
+        }
+    });
+});
+
 
 /* ----- CHANGE ACTIVE LINK ----- */
   
